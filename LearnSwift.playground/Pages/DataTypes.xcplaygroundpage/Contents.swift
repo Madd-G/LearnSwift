9da4455bc7d6f953bced835b3ea9fcc1deb34b4a
Madd-G <akhmadnuralamsyah@gmail.com>
2023-10-24 07:31:05 +0700

    feature: Data types

---
+++ LearnSwift.playground/Pages/DataTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+// String is "regular text"
+let myFirstItem: String = "Hello, world!"
+
+// Reference previously created objects
+let myTitle = myFirstItem
+
+// Boolean (aka Bool) is true or false
+let mySecondItem: Bool = true
+let myThirdItem: Bool = false
+
+// Do not do!
+//let myFourthItem: String = "true"
+
+// Swift is a type-safe language
+let myFifthItem: Bool = true
+let mySixthItem: String = "Hello, world!"
+let mySeventhItem = "Hello, world!"
+
+// Date
+let myDate: Date = Date()
+let myFirstDate: Date = .now
+
+
+// Number can be Int, Double, CGFloat & more
+
+// Int is a whole Integer
+let myInteger: Int = 1
+let myFloat: Float = 1.0
+
+// Use Double for math
+let myDouble: Double = 1.0
+
+// Use CGFloat for UI
+let myCGFloat: CGFloat = 1.0
